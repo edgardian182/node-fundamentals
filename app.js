1addae15e5fb8c86d3689f0934b858769768644e
Edgar Quintana <edgarquintana984@hotmail.com>
2019-01-22 19:14:27 -0500

    Adds CRUD functionality to Notes System

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
-console.log('Starting app...');
-
 const fs = require('fs');
 const _ = require('lodash');
-const notes = require('./notes.js');
 const yargs = require('yargs');
+
+const notes = require('./notes.js');
 
 const argv = yargs.argv
 
 let command = argv._[0];
-console.log('Command: ', command);
-console.log('Yargs', argv)
 
 if (command === 'add') {
-  notes.addNote(argv.title, argv.body);
+  let note = notes.addNote(argv.title, argv.body);
+  if (note) {
+    console.log('Note added successfully');
+    notes.logNote(note);
+  } else {
+    console.log('ERROR: Note title taken');
+  }
 } else if (command === 'list') {
-  notes.getAll();
+    let allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s)`);
+    allNotes.forEach( note => notes.logNote(note));
 } else if (command === 'read') {
-  notes.getNote(argv.title);
+    let note = notes.getNote(argv.title);
+    if (note) {
+      console.log('Note found');
+      notes.logNote(note);
+    } else {
+      console.log('Note not found');
+    }
 } else if (command === 'remove') {
-  notes.removeNote(argv.title);
+    let noteRemoved = notes.removeNote(argv.title);
+    let message = noteRemoved ? 'Note was removed' : 'Note not found';
+    console.log(message);
 } else {
-  console.log('Command not recognized')
+    console.log('Command not recognized')
 }
